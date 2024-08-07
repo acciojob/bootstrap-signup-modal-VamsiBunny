066b5bf0c17af08b1a5728ef1657b4e29b320c45
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-08-07 15:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+cy.get('button.btn-danger')
+  .click()
+  .then(() => {
+    cy.get('#signupModal')
+      .should('be.visible') // Check if modal is visible
+      .then(() => {
+        cy.get('.close')
+          .click() // Click the close button
+          .then(() => {
+            // Wait for the modal to become not visible
+            cy.get('#signupModal').should('not.be.visible', { timeout: 6000 });
+          });
+      });
+  });
